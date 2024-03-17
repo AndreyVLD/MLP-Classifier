72e7f28f6780d4c013ac2a6e1f8868f246421684
Andrei Vlad Nicula <94231732+AndreyVLD@users.noreply.github.com>
2024-03-17 22:21:12 +0100

    Added the intializers

---
+++ src/models/initializers.py
@@ -0,0 +1,46 @@
+import numpy as np
+import math
+
+def random_init(shape):
+    """
+        Args:
+            shape (tuple): the shape of the weights for one layer
+        Returns:
+            numpy.ndarray: The initialized weight matrix
+    """
+    return np.random.randn(*shape)
+
+
+def uniform_init(shape):
+    return np.random.uniform(-0.1, 0.1, shape)
+
+
+def he_normal_init(shape):
+    return np.random.randn(*shape) * np.sqrt(2 / shape[1])
+
+
+def lecun_normal_init(shape):
+    return np.random.randn(*shape) * np.sqrt(1 / shape[1])
+
+
+def xavier_normal_init(shape):
+    return np.random.randn(*shape) * np.sqrt(2 / (shape[1] + shape[0]))
+
+
+def xavier_uniform_init(shape):
+    val_range = math.sqrt(6) / math.sqrt(shape[0] + shape[1])
+    return np.random.uniform(-val_range, val_range, shape)
+
+
+def he_uniform_init(shape):
+    limit = math.sqrt(6 / shape[1])
+    return np.random.uniform(-limit, limit, shape)
+
+
+def orthogonal_init(shape):
+    random_matrix = np.random.randn(*shape)
+    U, _, Vt = np.linalg.svd(random_matrix, full_matrices=False)
+    if shape[0] > shape[1]:
+        return U
+    else:
+        return Vt
