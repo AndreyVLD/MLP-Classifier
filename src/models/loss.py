7a03495d76018bc32452afd044e4fcf550e5d8c8
Andrei Vlad Nicula <94231732+AndreyVLD@users.noreply.github.com>
2024-03-17 22:46:53 +0100

    Added Loss functions

--- src/models/loss.py
+++ src/models/loss.py
@@ -5,6 +5,23 @@ from abc import ABC, abstractmethod
 # Abstract
 class Loss(ABC):
     @abstractmethod
-    def calculate(self, y_true, y_pred):
+    def calculate(self, y_true: np.ndarray, y_pred: np.ndarray) -> (float, np.ndarray):
         """Calculates the loss value and its gradient."""
         pass
+
+
+class CategoricalCrossEntropy(Loss):
+
+    def calculate(self, y_true: np.ndarray, y_pred: np.ndarray) -> (float, np.ndarray):
+        y_pred = np.clip(y_pred, a_min=1e-7, a_max=1 - 1e-7)
+        grad = -(y_true / y_pred)
+        loss = - (y_true @ np.log(y_pred))
+
+        return loss, grad
+
+
+class MeanSquaredError(Loss):
+    def calculate(self, y_true: np.ndarray, y_pred: np.ndarray) -> (float, np.ndarray):
+        grad = 2 * (y_pred - y_true) / y_true.shape[1]
+        loss = np.mean(np.square(y_pred - y_true))
+        return loss, grad
